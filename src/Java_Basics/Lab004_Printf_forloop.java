55fafe91247c90f4d40ee39be99bb00517cc57c2
SriSruthi-K <kolliparasruthi@gmail.com>
2025-09-16 17:05:42 +0530

    Added new program for table of nine with printf and for loop

---
+++ src/Java_Basics/Lab004_Printf_forloop.java
@@ -0,0 +1,11 @@
+package Java_Basics;
+
+import java.util.Arrays;
+
+public class Lab004_Printf_forloop {
+    public static void main(String[] args) {
+        for (int i = 1; i  <=10; i++ ){
+            System.out.printf("9*%d= %d", i, i * 9);
+            System.out.println("");
+    }
+}}
