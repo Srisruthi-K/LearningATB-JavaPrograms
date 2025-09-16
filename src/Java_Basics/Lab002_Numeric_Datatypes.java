21a439b47efac1ef79a03443e71f84e91d6cb629
SriSruthi-K <kolliparasruthi@gmail.com>
2025-09-16 16:24:30 +0530

    Added new program

--- src/Java_Basics/Lab002_Numeric_Datatypes.java
+++ src/Java_Basics/Lab002_Numeric_Datatypes.java
@@ -1,13 +1,11 @@
 package Java_Basics;
 
-import com.sun.source.util.SourcePositions;
-
 public class Lab002_Numeric_Datatypes {
     public static void main(String[] args) {
         byte b = 100;
         int i = 2332455;
         short s = 4334;
-        long l =237235632953495l;
+        long l =237235632953495L;
         char c = 'S';
 
         float f1= 34.56789f;
@@ -16,6 +14,7 @@ public class Lab002_Numeric_Datatypes {
         System.out.println("short value s is "+ s);
         System.out.println("Long value l is" + l);
         System.out.println("Charcater value is "+ c);
+        System.out.println("float value f1 is "+ f1);
 
 
     }
