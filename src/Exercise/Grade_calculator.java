ae3e0e74e71f5ff7541967165dda76d24443cd21
SriSruthi-K <kolliparasruthi@gmail.com>
2025-10-11 07:15:01 +0530

    Added new files

---
+++ src/Exercise/Grade_calculator.java
@@ -0,0 +1,33 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Grade_calculator {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the score");
+        if(!sc.hasNextInt())
+        {
+            System.out.println("Invalid input.Enter the numerical score");
+            sc.close();
+            return;
+        }
+        int score= sc.nextInt();
+     if( score >=90 && score<=100) {
+         System.out.println("Grade : A");
+      }
+     else if (score >=80 && score<=89) {
+          System.out.println("Grade : B");
+     }
+     else if (score >=70 && score<=79) {
+         System.out.println("Grade : C");
+     }
+     else if (score >=60 && score<=69) {
+         System.out.println("Grade : D");
+     }
+     else if (score >=0 && score<=59) {
+         System.out.println("Grade : F");
+     }
+    }
+}
