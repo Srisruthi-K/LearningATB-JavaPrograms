befd4359d34355b07529f032e81b51fc7410d76f
SriSruthi-K <kolliparasruthi@gmail.com>
2025-10-26 08:57:58 +0530

    Added new files

---
+++ src/Exercise/Palindrome.java
@@ -0,0 +1,30 @@
+package Exercise;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid Input.Please enter the correct number");
+        }
+        int num = sc.nextInt();
+        int reverse = 0;
+        int original = num;
+
+        while (num > 0) {
+            int last = num % 10;
+            reverse = reverse * 10 + last;
+            num = num / 10;
+        }
+        if(original==reverse)
+        {
+            System.out.println("The number is a palindrome");
+        }
+        else
+        {
+            System.out.println("The umber is not a palindrome");
+        }
+
+    }
+}
