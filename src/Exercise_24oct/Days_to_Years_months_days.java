e71046d457b4b67520215e060faec73ef54e154c
SriSruthi-K <kolliparasruthi@gmail.com>
2025-10-30 17:46:21 +0530

    Added new files

---
+++ src/Exercise_24oct/Days_to_Years_months_days.java
@@ -0,0 +1,31 @@
+package Exercise_24oct;
+
+import java.util.Scanner;
+
+public class Days_to_Years_months_days {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the days");
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input.Enter valid days");
+        }
+        int input = sc.nextInt();
+        sc.close();
+        int years = 0;
+        int month = 0;
+        int fin_days = 0;
+        if (input - 365 >= 365) {
+            years = input/ 365;
+            input = input - (365 * years);
+        }
+        if (input >= 30) {
+            month = input /30;
+            input = input - (30 * month);
+        }
+        if (input < 30) {
+            fin_days = input;
+        }
+        System.out.println("The days entered is " + years + " years " + month+ "months " + fin_days +" days");
+
+    }
+}
---
+++ src/Exercise_24oct/Num_Divisible_5and11.java
@@ -0,0 +1,28 @@
+package Exercise_24oct;
+
+import java.util.Scanner;
+
+public class Num_Divisible_5and11 {
+    public static void main(String [] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        if(!sc.hasNextInt())
+        {
+            System.out.println("Invalid input.Enter a valid number");
+        }
+        int num = sc.nextInt();
+        sc.close();
+        if(num<=0)
+        {
+            System.out.println("Invalid input.Enter a valid postive number");
+            return;
+        } else if (num%5==0 && num%11 ==0) {
+            System.out.println("Number is divisible by both 5 and 11");
+            
+        }
+        else {
+            System.out.println("Number is not divisible by both 5 and 11");
+        }
+    }
+}
